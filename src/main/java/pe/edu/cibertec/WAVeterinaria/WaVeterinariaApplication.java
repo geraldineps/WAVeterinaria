ca15218787070fed2f36dad8ca7c3181c379a208
graldineps <geriifujioka@gmail.com>
2024-05-01 19:46:12 -0500

    estructura del proyecto

---
+++ src/main/java/pe/edu/cibertec/WAVeterinaria/WaVeterinariaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAVeterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaVeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaVeterinariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/WAVeterinaria/WaVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAVeterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaVeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
